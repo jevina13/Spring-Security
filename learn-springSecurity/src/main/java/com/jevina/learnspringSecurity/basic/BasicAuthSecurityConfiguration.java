d49afa4613bc395211216ce30901534079b57755
jevin13 <jevina131999@gmail.com>
2024-02-14 21:36:54 +0530

    initial commit

---
+++ learn-springSecurity/src/main/java/com/jevina/learnspringSecurity/basic/BasicAuthSecurityConfiguration.java
@@ -0,0 +1,97 @@
+package com.jevina.learnspringSecurity.basic;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+import javax.sql.DataSource;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.jdbc.JdbcDaoImpl;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+
+@Configuration
+public class BasicAuthSecurityConfiguration {
+
+	@Bean
+	SecurityFilterChain SecurityFilterChain(HttpSecurity http) throws Exception {
+		http.authorizeHttpRequests(
+				(requests) -> 
+					requests.anyRequest().authenticated());
+		http.sessionManagement(
+				session -> 
+					session.sessionCreationPolicy(
+							SessionCreationPolicy.STATELESS)
+				);
+		//http.formLogin(withDefaults());
+		http.httpBasic(withDefaults());
+		http.csrf(csrf -> csrf.disable());
+		
+		http.headers(headers -> headers.frameOptions(frameOptionsConfig-> frameOptionsConfig.disable()));
+		
+		return http.build();
+	}
+	
+//	@Bean
+//	public UserDetailsService userDetailService() {
+//		
+//		var user = User.withUsername("jevina")
+//			.password("{noop}dummy")	//{noop} means no encoding
+//			.roles("USER")
+//			.build();
+//
+//		
+//		var admin = User.withUsername("admin")
+//				.password("{noop}dummy")
+//				.roles("ADMIN")
+//				.build();
+//
+//		return new InMemoryUserDetailsManager(user, admin);
+//	}
+	
+	@Bean
+	public DataSource dataSource() {
+		return new EmbeddedDatabaseBuilder()
+				.setType(EmbeddedDatabaseType.H2)
+				.addScript(JdbcDaoImpl.DEFAULT_USER_SCHEMA_DDL_LOCATION)
+				.build();
+	}
+	
+	@Bean
+	public UserDetailsService userDetailService(DataSource dataSource) {
+		
+		var user = User.withUsername("jevina")
+			//.password("{noop}dummy")
+			.password("pass")
+			.passwordEncoder(str -> passwordEncoder().encode(str))
+			.roles("USER")
+			.build();
+		
+		var admin = User.withUsername("admin")
+				//.password("{noop}dummy")
+				.password("dummy")
+				.passwordEncoder(str -> passwordEncoder().encode(str))
+				.roles("ADMIN", "USER")
+				.build();
+		
+		var jdbcUserDetailsManager = new JdbcUserDetailsManager(dataSource);
+		jdbcUserDetailsManager.createUser(user);
+		jdbcUserDetailsManager.createUser(admin);
+
+		return jdbcUserDetailsManager;
+	}
+	
+	@Bean
+	public BCryptPasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+}
